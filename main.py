1611de75b2679d659541f11c1e5f5d12d19dac47
Araime <alvonsa09121985@gmail.com>
2021-06-06 17:25:23 +0400

    Download books with titles And also: 1. Completely rebuilt the script structure 2. Removed 2 functions: get_book_id, download_book 3. urllib_parse removed

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 import os
 import logging
 import requests
-from urllib.parse import urlsplit
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-def get_book_id(book_url):
-    splitted_url = urlsplit(book_url)
-    book_id = splitted_url.query.replace('id=', '')
-    return book_id
+def get_book_link(book_id):
+    url = f'https://tululu.org/txt.php'
+    payload = {'id': book_id}
+    response = requests.get(url, params=payload)
+    check_for_redirect(response)
+    book_link = response.url
+    return book_link
 
 
 def check_for_redirect(response):
-    redirects = response.history
-    if redirects:
+    history_of_redirects = response.history
+    if history_of_redirects:
         raise requests.HTTPError
 
 
-def get_book_link():
-    books_urls = []
-    for i in range(1, 11):
-        url = f'https://tululu.org/txt.php?id={i}'
-        books_urls.append(url)
-    return books_urls
-
-
-def download_book(book_id, directory):
-    url = f'https://tululu.org/txt.php'
-    payload = {
-        'id': book_id
-    }
-    response = requests.get(url, params=payload)
+def parse_book_page(book_id):
+    book_page_link = f'https://tululu.org/b{book_id}'
+    response = requests.get(book_page_link)
     response.raise_for_status()
-    check_for_redirect(response)
-    filename = f'{book_id}.txt'
-    with open(f'{directory}{filename}', 'wb') as file:
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('h1')
+    title = title_tag.text.split('::')
+    book_name = title[0].strip()
+    author = title[1].strip()
+    book_page_information = {
+        'book_name': book_name,
+        'author': author
+    }
+    return book_page_information
+
+
+def download_txt(book_id, book_link, book_page_info, folder='books/'):
+    clean_filename = sanitize_filename(book_page_info['book_name'])
+    book_path = os.path.join(folder, f'{book_id}. {clean_filename}.txt')
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(book_link)
+    response.raise_for_status()
+    with open(book_path, 'wb') as file:
         file.write(response.content)
 
 
@@ -42,16 +51,11 @@ if __name__ == '__main__':
                         format='%(filename)s - %(levelname)s - %(message)s',
                         level=logging.ERROR)
 
-    directory = f'{os.getcwd()}/books/'
-    os.makedirs(directory, exist_ok=True)
-
-    books_links = get_book_link()
-
-    for book_link in books_links:
+    for book_id in range(1, 11):
         try:
-            book_id = get_book_id(book_link)
-            download_book(book_id, directory)
+            book_link = get_book_link(book_id)
+            book_page_info = parse_book_page(book_id)
+            download_txt(book_id, book_link, book_page_info)
         except requests.HTTPError:
-            logging.error('Данного id не существует')
+            logging.error(f'Книги с id № {book_id} не существует')
             continue
-
